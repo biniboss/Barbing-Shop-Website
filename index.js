a70014bd98c5cf54072e3a12860e3d4aa71fa795
binnyboss <ezeakonachinonso@gmail.com>
2020-02-15 16:47:18 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function clickMe(){
+	document.getElementById("menu").classList.toggle("change");
+
+	document.getElementById("nav").classList.toggle("change");
+}
+
+function onload(){
+	document.getElementById("hurray").style.display="block";
+}
